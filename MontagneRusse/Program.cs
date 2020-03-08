7e2b3af13a5a54c3d244965cbffa759cf9db3848
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2020-03-08 13:24:44 +0100

    Init project

---
+++ MontagneRusse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontagneRusse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
