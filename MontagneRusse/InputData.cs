7d8e45d7623a1cccf6ba488272cd064e49f11cd0
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2020-03-08 20:41:08 +0100

    Passing tests 1-5

---
+++ MontagneRusse/InputData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MontagneRusse
+{
+    public static class InputData
+    {
+
+        public static Func<List<string>> GetStage(int i)
+        {
+            if (i == 1)
+                return Stage1;
+            else if (i == 2)
+                return Stage2;
+            else if (i == 3)
+                return Stage3;
+            else if (i == 4)
+                return Stage4;
+            else if (i == 5)
+                return Stage5;
+            else if (i == 6)
+                return Stage6;
+            else
+                return null;
+        }
+
+        public static string GetInput(int i)
+        {
+            if (i == 1)
+                return "3 3 4";
+            else if (i == 2)
+                return "5 3 4";
+            else if (i == 3)
+                return "10 100 1";
+            else if (i == 4)
+                return "10000 10 5";
+            else if (i == 5)
+                return "100000 50000 1000";
+            else if (i == 6)
+                return "10000000 9000000 1000";
+            else
+                return null;
+        }
+
+        public static int GetResult(int i)
+        {
+            if (i == 1)
+                return 7;
+            else if (i == 2)
+                return 14;
+            else if (i == 3)
+                return 100;
+            else if (i == 4)
+                return 15000;
+            else
+                return -1;
+        }
+
+        private static List<string> Stage1()
+        {
+            var r = new List<string> { "3", "1", "1", "2" };
+            return r;
+        }
+        private static List<string> Stage2()
+        {
+            var r = new List<string> { "2", "3", "5", "4" };
+            return r;
+        }
+        private static List<string> Stage3()
+        {
+            var r = new List<string> { "1" };
+            return r;
+        }
+        private static List<string> Stage4()
+        {
+            var r = new List<string> { "100", "200", "300", "400", "500" };
+            return r;
+        }
+        private static List<string> Stage5()
+        {
+            var r = Enumerable.Range(0, 1000)
+                .Select(i => i % 2 == 0 ? "100000" : "99999")
+                .ToList();
+            return r;
+        }
+
+        private static List<string> Stage6()
+        {
+            var rnd = new Random();
+            var r = Enumerable.Range(0, 1000)
+                .Select(i => rnd.Next(10000, 100000).ToString())
+                .ToList();
+            return r;
+        }
+    }
+}
--- MontagneRusse/Program.cs
+++ MontagneRusse/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace MontagneRusse
 {
@@ -6,7 +10,117 @@ namespace MontagneRusse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var currentStage = 6;
+#if (!DEBUG)
+            string[] inputs = Console.ReadLine().Split(' ');
+#else
+            string[] inputs = InputData.GetInput(currentStage).Split(' ');
+#endif
+
+            int NumberOfSeats = int.Parse(inputs[0]);
+            int Capacity = int.Parse(inputs[1]);
+            int NumberOfGroupe = int.Parse(inputs[2]);
+
+            var queue = new Queue<Group>();
+            Enumerable.Range(0, NumberOfGroupe)
+                .ToList()
+                .ForEach(i => {
+#if (!DEBUG)
+                    queue.Enqueue(new Group { Lenght = int.Parse(Console.ReadLine()) } );
+#else
+                    queue.Enqueue(new Group { Lenght = int.Parse(InputData.GetStage(currentStage)()[i]) });
+#endif
+                });
+
+            var rideService = new RideService { Ride = new Ride { MaxCapacity = Capacity, MaxNumberOfSeats = NumberOfSeats }, Queue = queue };
+            rideService.Previsualisation();
+            rideService.DoWork();
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine(rideService.Money);
         }
     }
+
+    public class Ride
+    {
+        /// <summary>
+        /// Le nombre de tour possible
+        /// </summary>
+        public int MaxCapacity { get; set; }
+        /// <summary>
+        /// Le nombre de siège disponible
+        /// </summary>
+        public int MaxNumberOfSeats { get; set; }
+    }
+
+    public class Group
+    {
+        public bool OnRide { get; set; } = false;
+        public int Lenght { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+
+    public class RideService
+    {
+        /// <summary>
+        /// Le mananège
+        /// </summary>
+        public Ride Ride { get; set; }
+        /// <summary>
+        /// L'argent gagné
+        /// </summary>
+        public long Money { get; set; } = 0;
+        /// <summary>
+        /// La file d'attente
+        /// </summary>
+        public Queue<Group> Queue { get; set; }
+
+        public int NumberOfTurn { get; private set; } = 0;
+        public int AvailableSeat { get; private set; }
+        
+        public void Previsualisation()
+        {
+            char quote = '"';
+            Console.Error.WriteLine($"MaxNumberOfSeats : {Ride.MaxNumberOfSeats} - MaxCapacity : {Ride.MaxCapacity} - NbGroup : {Queue.Count}");
+            // Console.Error.WriteLine($"MaxNumberOfSeats : {Ride.MaxNumberOfSeats} - MaxCapacity : {Ride.MaxCapacity} - NbGroup : {Queue.Count} - Queue {String.Join(", ", Queue.ToList().Select(i => $"{quote}{i.Lenght}{quote}"))}");
+        }
+
+        public void DoWork()
+        {
+            // https://fr.wikipedia.org/wiki/Algorithme_du_li%C3%A8vre_et_de_la_tortue
+
+
+            var sw = new Stopwatch();
+            sw.Start();
+            var hs = new HashSet<Guid>();
+            for (long i = 0; i < Ride.MaxCapacity; i++)
+            {
+                // Console.Error.WriteLine($"({i}) New turn will start !");
+                Reset();
+                hs.Clear();
+                while(AvailableSeat > 0 && Queue.Peek().Lenght <= AvailableSeat && hs.Add(Queue.Peek().Id))
+                {
+                    var currentGroup = Queue.Dequeue();
+                    AvailableSeat -= currentGroup.Lenght;
+                    Money += currentGroup.Lenght;
+                    Queue.Enqueue(currentGroup);
+                    hs.Add(currentGroup.Id);
+                    // Console.Error.WriteLine($"{currentGroup.Lenght} get in the ride ");
+                }
+                // Console.Error.WriteLine($"Ellapsed time for adding 1 group : {sw.Elapsed.TotalSeconds}");
+
+            }
+            sw.Stop();
+            Console.Error.WriteLine($"Ellapsed time : {sw.Elapsed.TotalSeconds}");
+        }
+
+        private void Reset()
+        {
+            this.AvailableSeat = this.Ride.MaxNumberOfSeats;
+            // trop lent
+            //Queue.ToList().ForEach(g => g.OnRide = false);
+        }
+    }
+     
 }
